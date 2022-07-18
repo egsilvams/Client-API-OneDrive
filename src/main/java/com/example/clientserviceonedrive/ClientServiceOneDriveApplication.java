7aeada4065b90686a66d0544113f35e4874a4bb4
Evandro <45930546+egsilvams@users.noreply.github.com>
2022-07-17 22:19:19 -0400

    Add files via upload

---
+++ src/main/java/com/example/clientserviceonedrive/ClientServiceOneDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientserviceonedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServiceOneDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientServiceOneDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clientserviceonedrive/ClientServiceOneDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientserviceonedrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceOneDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
